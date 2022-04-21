4d0bcdd269fafe9c03edb21e056ee15c06969a44
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-22 05:46:33 +0600

    data send from client to server

--- client/src/component/User/User.js
+++ client/src/component/User/User.js
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from 'react';
 
 const User = () => {
-const [user,setUser]=useState([])
+const [users,setUser]=useState([])
 useEffect(()=>{
   fetch('http://localhost:5000/users')
   .then(res=>res.json())
   .then(data=>setUser(data))
 },[])
-
+const handleFormSubmit = (event)=>{
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+     const user = {name, email}
+      
+    fetch('http://localhost:5000/user',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=> console.log(data))
+    event.preventDefault()
+}
     return (
         <div>
-            <h2>here user have {user.length}</h2>
+            <h2>here user have {users.length}</h2>
+            <form onSubmit={handleFormSubmit}>
+                <input type="text" name="name" placeholder='name' id="" />
+                <input type="email" name="email" placeholder='email' id="" />
+                <input type="submit" value="submit" />
+            </form>
+            <ul>
+                {
+                    users.map(user=> <li key={user.id}>{user.name}</li>)
+                }
+            </ul>
         </div>
     );
 };
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(cors())
+app.use(express.json())
+
 
 const users = [
     {id:1, name:"alam",email:"alam@gmail.com"},
@@ -21,6 +23,12 @@ app.get('/about',(req,res)=>{
 })
 app.get('/users',(req,res)=>{
     res.send(users)
+})
+
+app.post('/user',(req,res)=>{
+    console.log(req.body)
+    
+    res.send('successful')
 })
 // this is dynamic param
 app.get('/user/:id',(req,res)=>{
